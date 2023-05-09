e1169070144b7abf5305034bb4a56879b1a8725c
hrk-0830 <ponte.di.ingresso11@gmail.com>
2023-05-09 13:25:20 +0000

    CreateController

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "homes#top"
   resources :posts, only: [:new,:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
-  get 'user/show'
-  get 'user/edit'
-  get 'user/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
